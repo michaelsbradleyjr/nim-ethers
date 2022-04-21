2f8a8bb1810ec061c263bf26e3ba073cf1e705b9
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2022-04-21 12:40:26 -0500

    .

--- ethers/transaction.nim
+++ ethers/transaction.nim
@@ -1,14 +1,39 @@
 import pkg/stew/byteutils
 import ./basics
 
-type Transaction* = object
-  sender*: ?Address
-  to*: Address
-  data*: seq[byte]
-  nonce*: ?UInt256
-  chainId*: ?UInt256
-  gasPrice*: ?UInt256
-  gasLimit*: ?UInt256
+type
+  Transaction* = object of RootObj
+    sender*: ?Address
+    to*: Address
+    data*: seq[byte]
+    nonce*: ?UInt256
+    chainId*: ?UInt256
+    gasPrice*: ?UInt256
+    gasLimit*: ?UInt256
+
+  TransactionResponse* = ref object of Transaction
+    blockNumber*: ?UInt256
+    blockHash*: ?array[32, byte]
+    timestamp*: ?UInt256
+    confirmations*: Natural
+    raw*: ?seq[byte]
+
+  TransactionReceipt* = object
+    sender*: ?Address
+    to*: ?Address
+    contractAddress*: ?Address
+    transactionIndex*: Natural
+    gasUsed*: UInt256
+    logsBloom*: seq[byte]
+    blockHash*: array[32, byte]
+    transactionHash*: array[32, byte]
+    logs*: seq[Log]
+    blockNumber*: Natural
+    confirmations*: Natural
+    cumulativeGasUsed*: UInt256
+    byzantium*: bool
+    status*: bool
+
 
 func `$`*(transaction: Transaction): string =
   result = "("
